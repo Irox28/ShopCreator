6ae28efd6641be8958a3fc35f6c4fb6a2cd0a670
Irox <iroxhightech@gmail.com>
2025-03-23 15:35:22 +0100

    Commit

---
+++ server/sv.lua
@@ -0,0 +1,11 @@
+RegisterNetEvent("addItem")
+AddEventHandler("addItem", function(name, price)
+    local _src = source
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    if xPlayer.getAccount('black_money').money < price then
+        TriggerClientEvent("okokNotify:Alert", _src, "Achat impossible", "Vous n'avez pas assez d'argent sale !", 5000, "error")
+    else 
+        xPlayer.removeAccountMoney('black_money', price)
+        xPlayer.addInventoryItem(name, 1)
+    end
+end)
