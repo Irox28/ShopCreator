1c717ace27b1d0fbe4a790966698aabc7f30e1c1
Irox <iroxhightech@gmail.com>
2025-03-23 15:50:11 +0100

    Supprimer le dossier

--- IroxShopCreator/server/sv.lua
+++ IroxShopCreator/server/sv.lua
@@ -1,11 +1,12 @@
 RegisterNetEvent("addItem")
-AddEventHandler("addItem", function(name, price)
+AddEventHandler("addItem", function(name, price, quantity)
     local _src = source
     local xPlayer = ESX.GetPlayerFromId(_src)
     if xPlayer.getAccount('black_money').money < price then
         TriggerClientEvent("okokNotify:Alert", _src, "Achat impossible", "Vous n'avez pas assez d'argent sale !", 5000, "error")
     else 
         xPlayer.removeAccountMoney('black_money', price)
-        xPlayer.addInventoryItem(name, 1)
+        xPlayer.addInventoryItem(name, quantity)
+        TriggerClientEvent("okokNotify:Alert", _src, "Achat réussi", "Vous avez acheté avec succès " .. quantity .. "x " .. name .. " pour $" .. price .. ".", 5000, "success")
     end
 end)
